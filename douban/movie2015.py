3aa51bf8e1ff08c1bf99970ae17322904b16880a
Dectinc Chen <chensj009@gmail.com>
2015-12-29 14:54:07 +0800

    add source cache

--- douban/movie2015.py
+++ douban/movie2015.py
@@ -5,8 +5,6 @@
 # @date     2015-12-29 13:12
 
 import re
-import lxml.etree as le
-import cStringIO
 
 
 class Movie(object):
@@ -22,17 +20,35 @@ class Movie(object):
 def load_source(_url='http://movie.douban.com/annual2015'):
     import requests
     response = requests.get(_url)
+    print response.status_code
     if response.status_code == 200:
         return response.content
     else:
-        return ''
+        f = open('source', 'rb')
+        content = ''.join(f.readlines())
+        f.close()
+        return content
 
 
 def get_section():
-    doc = le.parse(page_content)
-    section_path = '//*[@id="main"]/div[contains(@class, "section")]'
-    sections = doc.xpath(section_path)
-    print(map(le.tostring, sections))
+    p_section = re.compile(
+            r'<div class="section .*?<div class="barrage-form-section">')
+    sections = p_section.findall(page_content)
+    print len(sections)
+    for section in sections:
+        section_type = get_section_type(section)
+        if section_type == 'top-10-widget':
+            parse_top_10_widget(section)
+        elif section_type == 'dialogue-widget':
+            parse_dialog_widget(section_type)
+        else:
+            pass
+
+
+def get_section_type(section):
+    p_section_type = re.compile(r'(?<=<div class="section )[a-z0-9-]+')
+    section_type = p_section_type.search(section)
+    return section_type.group()
 
 
 def parse_top_10_widget(_content):
@@ -50,5 +66,7 @@ def print_list(_file='douban_movie_annual_2015'):
 
 if __name__ == '__main__':
     page_content = load_source()
+    page_content = page_content.replace('\r\n', ' ')
+    page_content = page_content.replace('\n', ' ')
     list_ = get_section()
     print_list()
